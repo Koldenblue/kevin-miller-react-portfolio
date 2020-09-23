8d04f6ab9ca7c90c219319828137e2d9782a4e4a
Kevin Miller <kmillergit@outlook.com>
2020-09-23 09:38:22 -0700

    set up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+// the bootstrap stylesheet should be imported first, so that the custom styles cascade and take precedence
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/styles/style.css"
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Button className='myBtn' variant="primary">Primary</Button>{' '}
     </div>
   );
 }
