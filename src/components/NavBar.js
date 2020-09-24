f4ea127c02fdac790c907bcac03c9b9614f6fd93
Kevin Miller <kmillergit@outlook.com>
2020-09-23 23:31:25 -0700

    fixed up navbar, imported images, added htmls to edit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,24 +9,17 @@ import FormControl from 'react-bootstrap/FormControl'
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="index.html"> Kevin Miller, PhD - <span id="portfolio">Portfolio Home</span><span class="sr-only">(current)</span></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="portfolio.html">Portfolio</Nav.Link>
+          <Nav.Link href="resume.html">Resume</Nav.Link>
+          <Nav.Link href="chemistry.html">Chemistry</Nav.Link>
+          <Nav.Link href="pictures.html">Pictures</Nav.Link>
+          <Nav.Link href="contact.html">Contact</Nav.Link>
+          <Nav.Link href="about-me.html">About Me</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   )
