76c4729dbfe2e6a49177bc592a76aba93e34c2f8
Kevin Miller <kmillergit@outlook.com>
2020-10-29 20:03:43 -0700

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,7 +7,7 @@ class Resume extends React.Component {
       <>
         <Background image='../assets/images/beach-hut.jpg' />
         <main class="container nav-avoid bottom-space" id="resume-container">
-          <iframe src={require("../assets/images/Kevin-Miller-Resume-FE-10-21-20.pdf")} id='resume-pdf'></iframe>
+          <iframe src={require("../assets/images/Kevin-Miller-Resume.pdf")} id='resume-pdf'></iframe>
         </main>
       </>
     )
