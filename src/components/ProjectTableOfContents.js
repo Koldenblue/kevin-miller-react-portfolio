d136bce215cadf8244b41f9905131b742b6c7164
Kevin Miller <kmillergit@outlook.com>
2020-11-08 12:33:21 -0800

    sun animation complete

--- src/components/ProjectTableOfContents.js
+++ src/components/ProjectTableOfContents.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import ListGroup from "react-bootstrap/ListGroup";
-import Nav from "react-bootstrap/Nav";
 
 export default function ProjectTableOfContents() {
-  let styles = {
-    tableOfContents: {
-      backgroundColor: 'red',
-      height: '200px',
-      zIndex: '9999',
-      position: 'fixed'
-    }
-  }
-
   return (<>
     <ul className='list-nav'>
   <h3 className='top-link'>Projects</h3>
@@ -35,23 +24,6 @@ export default function ProjectTableOfContents() {
       <li><a href='#connect'>Connect 4</a></li>
 
     </ul>
-    {/* <Nav fill defaultActiveKey="/home" className="project-nav flex-column">
-      <Nav.Link className='top-link' href="#connect4">Active</Nav.Link>
-      <Nav.Link href=''>Quick Readme Generator</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Node Python SQL Database Manager</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href=''>Link</Nav.Link>
-      <Nav.Link href='#connect4'>Connect 4</Nav.Link>
-    </Nav> */}
-
 
     {/* <ListGroup className='table-contents'>
       <ListGroup.Item>Cras justo odio</ListGroup.Item>
---
+++ src/components/Sun/Sun.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sun.css';
+
+export default function Sun() {
+
+  return (<>
+    <div className='anim-sun'></div>
+    <div className='sun-rays'></div>
+  </>)
+}
