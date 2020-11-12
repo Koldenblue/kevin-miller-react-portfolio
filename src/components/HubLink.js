96797d67bd556a31003adc66a8d1f1148ffecb8a
Kevin Miller <kmillergit@outlook.com>
2020-11-11 20:50:47 -0800

    added contact button to nav

--- src/components/HubLink.js
+++ src/components/HubLink.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from 'react-bootstrap/Image'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function HubLink() {
   let styles = {
@@ -15,8 +17,14 @@ function HubLink() {
   }
   return (
     <>
+        <DropdownButton className='dropdown' title='Contact' drop='left' variant="info" id="dropdown-contact">
+          <Dropdown.Item disabled>Email: klmille@gmail.com</Dropdown.Item>
+          <Dropdown.Item disabled>Cell Phone: (310) 562-4572</Dropdown.Item>
+        </DropdownButton>
+
       <a href="https://www.linkedin.com/in/kevin-miller-phd-1b197986/" ><Image src={require('../assets/images/LI-In-Bug.png')} fluid style={styles.linkedIn} /></a>
-      <a href="https://github.com/Koldenblue"><Image src={require('../assets/images/gitMark32.png')} fluid style={styles.git}/></a>
+
+      <a href="https://github.com/Koldenblue"><Image src={require('../assets/images/gitMark32.png')} fluid style={styles.git} /></a>
     </>
   )
 }
