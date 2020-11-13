bb868452f7c083cdf93e2d034cef6535edde3e31
Kevin Miller <kmillergit@outlook.com>
2020-11-12 22:49:32 -0800

    animated birds

--- src/components/Stars/StarBg.js
+++ src/components/Stars/StarBg.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import range from 'lodash-es/range'
 import './styles.css'
+import Background from "../Background";
 
-const items = range(20)
-const interp = i => r => `translate3d(0, ${15 * Math.sin(r + (i * 2 * Math.PI) / 1.6)}px, 0)`
+const items = range(15)
+const interp = i => r => `translate3d(0, ${15 * Math.sin(r + (i * 2 * Math.PI) / 1.6)}px, 0`
 
 export default function StarBg() {
 
@@ -13,26 +14,30 @@ export default function StarBg() {
       while (1) await next({ radians: 2 * Math.PI })
     },
     from: { radians: 0 },
-    config: { duration: 3500 },
+    config: { duration: 7000 },
     reset: true,
   })
 
-  return (
+  return (<>
+    <Background image='../assets/images/tidepool.jpg' />
+
     <div id='star-bars'>
       {items.map(i => {
-        if (i % 3 === 0) {
+        // if (i % 3 === 0) {
           return(<>
             <animated.div key={i} className="script-bf-box" style={{ transform: radians.interpolate(interp(i)) }} />
+            <animated.div key={-i} className="script-bf-box-2" style={{ transform: radians.interpolate(interp(i)) }} />
+
           </>)
-        } 
-        else {
-          return (<>
-            <div key={i} className="script-bf-none"/>
-          </>)
-        }
+        // } 
+        // else {
+        //   return (<>
+        //     <div key={i} className="script-bf-none"/>
+        //   </>)
+        // }
       })}
 
     </div>
-  )
+  </>)
 }
 
