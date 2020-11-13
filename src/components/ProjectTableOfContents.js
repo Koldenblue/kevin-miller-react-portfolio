e99a800c8c1dbc85801db9db9a94c0b4b9811067
Kevin Miller <kmillergit@outlook.com>
2020-11-13 15:10:43 -0800

    smooth scrolling on portfolio

--- src/components/ProjectTableOfContents.js
+++ src/components/ProjectTableOfContents.js
@@ -8,29 +8,34 @@ export default function ProjectTableOfContents(props) {
     console.log(props.chessRef.current)
   },[])
 
+  const smoothScroll = (event, ref) => {
+    event.preventDefault();
+    ref.current.scrollIntoView({behavior: 'smooth'})
+  }
+
   return (<>
     <ul className='list-nav'>
       <h3 className='top-link'>Projects</h3>
-      <li><a href='#color-rippler'>Color Rippler</a></li>
-      <li><a href='#bartender'>Express Bartender</a></li>
-      <li><a href='#inform-react'>Inform and React</a></li>
-      <li><a href='#para-bub'>Para Bubbles</a></li>
-      <li><a href='#readme-gen'>Readme Generator</a></li>
-      <li><a href='#mern-template'>MERN-Redux Template</a></li>
-      <li><a href='#hamburger-handlebars'>Hamburger Handlebars</a></li>
-      <li><a href='#redux-journal'>Redux Journal</a></li>
-      <li><a href='#chess'>Chess</a></li>
-      <li><a href='#emp-dir'>Mock Employee Directory</a></li>
-      <li><a href='#database-manager'>Database Manager</a></li>
-      <li><a href='#trek-app'>Trek</a></li>
-      <li><a href='#quiz-template'>Quiz Template</a></li>
-      <li><a href='#weather-forecaster'>Weather Forecaster</a></li>
-      <li><a href='#notetaker'>Note Taker</a></li>
-      <li><a href='#pass-gen'>Password Generator</a></li>
-      <li><a href='#template-engine'>Template Engine</a></li>
-      <li><a href='#workday-scheduler'>Workday Scheduler</a></li>
-      <li><a href='#balance-sheet'>Balance Sheet Reader</a></li>
-      <li id='last-link'><a href='#connect'>Connect 4</a></li>
+      <li><a href='#color-rippler' onClick={(event) => smoothScroll(event, props.ripplerRef)}>Color Rippler</a></li>
+      <li><a href='#bartender' onClick={(event) => smoothScroll(event, props.bartenderRef)}>Express Bartender</a></li>
+      <li><a href='#inform-react' onClick={(event) => smoothScroll(event, props.informRef)}>Inform and React</a></li>
+      <li><a href='#para-bub' onClick={(event) => smoothScroll(event, props.parabubRef)}>Para Bubbles</a></li>
+      <li><a href='#readme-gen' onClick={(event) => smoothScroll(event, props.readmeRef)}>Readme Generator</a></li>
+      <li><a href='#mern-template' onClick={(event) => smoothScroll(event, props.mernTemplateRef)}>MERN-Redux Template</a></li>
+      <li><a href='#hamburger-handlebars' onClick={(event) => smoothScroll(event, props.hamRef)}>Hamburger Handlebars</a></li>
+      <li><a href='#redux-journal' onClick={(event) => smoothScroll(event, props.journalRef)}>Redux Journal</a></li>
+      <li><a href='#chess' onClick={(event) => smoothScroll(event, props.chessRef)}>Chess</a></li>
+      <li><a href='#emp-dir' onClick={(event) => smoothScroll(event, props.empFinderRef)}>Mock Employee Directory</a></li>
+      <li><a href='#database-manager' onClick={(event) => smoothScroll(event, props.managerRef)}>Database Manager</a></li>
+      <li><a href='#trek-app' onClick={(event) => smoothScroll(event, props.trekRef)}>Trek</a></li>
+      <li><a href='#quiz-template' onClick={(event) => smoothScroll(event, props.quizRef)}>Quiz Template</a></li>
+      <li><a href='#weather-forecaster' onClick={(event) => smoothScroll(event, props.weatherRef)}>Weather Forecaster</a></li>
+      <li><a href='#notetaker' onClick={(event) => smoothScroll(event, props.noteRef)}>Note Taker</a></li>
+      <li><a href='#pass-gen' onClick={(event) => smoothScroll(event, props.passRef)}>Password Generator</a></li>
+      <li><a href='#template-engine' onClick={(event) => smoothScroll(event, props.templateRef)}>Template Engine</a></li>
+      <li><a href='#workday-scheduler' onClick={(event) => smoothScroll(event, props.workdayRef)}>Workday Scheduler</a></li>
+      <li><a href='#balance-sheet' onClick={(event) => smoothScroll(event, props.balanceRef)}>Balance Sheet Reader</a></li>
+      <li id='last-link'><a href='#connect' onClick={(event) => smoothScroll(event, props.connectRef)}>Connect 4</a></li>
     </ul>
   </>)
 }
